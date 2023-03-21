80793f9f64cf3e52df4f96ead116931afeb266fd
AntonC9018 <anton.curm@gmail.com>
2023-03-21 21:41:34 +0200

    minimal graphql

---
+++ GraphQL.cs
@@ -0,0 +1,34 @@
+﻿using HotChocolate;
+
+namespace efcore_transactions;
+
+public class Query
+{
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<Person> Person([Service] ApplicationDbContext context) =>
+        context.Set<Person>();
+    
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<Project> Project([Service] ApplicationDbContext context) =>
+        context.Set<Project>();
+}
+
+public class PersonType : ObjectType<Person>
+{
+    protected override void Configure(IObjectTypeDescriptor<Person> descriptor)
+    {
+        descriptor.BindFieldsImplicitly();
+    }
+}
+
+public class ProjectType : ObjectType<Project>
+{
+    protected override void Configure(IObjectTypeDescriptor<Project> descriptor)
+    {
+        descriptor.BindFieldsImplicitly();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using efcore_transactions;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
+using efcore_transactions;
+using HotChocolate.Execution.Processing;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,121 +21,24 @@ builder.Services.AddAutoMapper(c =>
 });
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+{
+    var graph = builder.Services.AddGraphQLServer();
+    graph.AddGraphQLServer();
+    graph.AddQueryType<Query>();
+    graph.AddProjections();
+    graph.AddFiltering();
+    graph.AddSorting();
+    graph.InitializeOnStartup();
+    graph.AddTransactionScopeHandler<DefaultTransactionScopeHandler>();
+    graph.AddMutationConventions(applyToAllMutations: true);
+    graph.AddType<ProjectType>();
+    graph.AddType<PersonType>();
+}
 
 var app = builder.Build();
 
-app.UseDeveloperExceptionPage();
-app.UseSwagger();
-app.UseSwaggerUI(o =>
-{
-    o.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    o.RoutePrefix = string.Empty;
-});
-
-app.MapGet("", () =>
-{
-    return "Hello";
-});
-
-app.MapGet("/people/{id?}", async (
-    [FromServices] ApplicationDbContext data,
-    [FromServices] IMapper mapper,
-    int? id) =>
-{
-    IQueryable<Person> q = data.Set<Person>()
-        .Include(p => p.Projects);
-
-    if (id is not null)
-        q = q.Where(p => p.Id == id.Value);
-
-    var dtoq = mapper.ProjectTo<PersonResponseDto>(q);
-    
-    return await dtoq.ToListAsync();
-});
-
-app.MapPost("/people", async(
-    [FromServices] ApplicationDbContext data,
-    [FromBody] PersonRequestDto person,
-    [FromServices] IMapper mapper) =>
-{
-    Person entity;
-    if (person.Id == default)
-    {
-        entity = mapper.Map<Person>(person);
-        data.ChangeTracker.TrackGraph(entity, e =>
-        {
-            e.Entry.State = EntityState.Added;
-        });
-    }
-    else
-    {
-        person.Projects.Sort((a, b) =>
-        {
-            var v0 = a.Id ?? default;
-            var v1 = b.Id ?? default;
-            return v1.CompareTo(v0);
-        });
-        
-        var projectIds = person.Projects
-            .Select(p => p.Id)
-            .SelectNonZeroValue()
-            .ToHashSet();
-        
-        entity = await data.Set<Person>()
-            .Include(p => p.Projects
-                .OrderBy(proj => -proj.Id)
-                .Where(proj => projectIds.Contains(proj.Id)))
-            .FirstAsync(p => p.Id == person.Id);
-
-        if (entity.Projects.Count != projectIds.Count)
-            throw new Exception("Invalid project ids present in request.");
-        
-        mapper.Map(person, entity);
-        
-        data.ChangeTracker.TrackGraph(entity, e =>
-        {
-            if (e.Entry.IsKeySet)
-                e.Entry.State = EntityState.Modified;
-            else
-                e.Entry.State = EntityState.Added;
-        });
-    }
-    
-    await data.SaveChangesAsync();
-    return mapper.Map<PersonResponseDto>(entity);
-});
-
-
-app.MapGet("names", async (
-    [FromServices] ApplicationDbContext context,
-    [FromServices] IMapper mapper) =>
-{
-
-    var q = context.Set<Project>()
-        .Select(x => new { Value = x, Name = x.ProjectName })
-        .Where(x => x.Name == "A")
-        .Select(x => x.Value.Id);
-    return await q.ToListAsync();
-    // var q1 = context
-    //     .Set<Project>()
-    //     .ProjectTo<NameDto>(mapper.ConfigurationProvider)
-    //     .Where(x => x.Name == "A")
-    //     .ProjectTo<Project>(mapper.ConfigurationProvider);
-    // return await q1.ToListAsync();
-    // return new
-    // {
-    //     @interface = new
-    //     {
-    //         people = await context.Set<Person>().SelectNameByInterface().ToListAsync(),
-    //         projects = await context.Set<Project>().SelectNameByInterface().ToListAsync()  
-    //     },
-    //     generic = new
-    //     {
-    //         people = await context.Set<Person>().SelectNameByInterfaceGeneric().ToListAsync(),
-    //         projects = await context.Set<Project>().SelectNameByInterfaceGeneric().ToListAsync()  
-    //     },
-    // };
-});
+app.MapGraphQL();
 
 app.Run();
