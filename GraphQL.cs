d924636f36fd7c92bb45468003c1587066018c56
AntonC9018 <anton.curm@gmail.com>
2023-03-29 21:51:20 +0300

    russian enum type

--- GraphQL.cs
+++ GraphQL.cs
@@ -1,126 +1,24 @@
-﻿using System.Reflection;
-using AutoMapper.QueryableExtensions;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿namespace efcore_transactions;
 
-namespace efcore_transactions;
-
-[Flags]
-public enum MiddlewareFlags
+public enum Статус
 {
-    Paging = 1 << 0,
-    Projection = 1 << 1,
-    Filtering = 1 << 2,
-    Sorting = 1 << 3,
-    All = Paging | Projection | Filtering | Sorting,
+    Активный,
+    Завершенный,
 }
 
-public static class GraphQlHelper
+public class StatusType : EnumType<Статус>
 {
-    public static IObjectFieldDescriptor EfQueryEntity<TEntity>(
-        this IObjectTypeDescriptor descriptor,
-        string? name = null,
-        MiddlewareFlags middlewareFlags = MiddlewareFlags.All)
-    
-        where TEntity : class
+    protected override void Configure(IEnumTypeDescriptor<Статус> descriptor)
     {
-        return EfQuery<TEntity, EfObjectType<TEntity>>(descriptor, name, middlewareFlags);
+        descriptor.Name("Status");
+        descriptor.Value(Статус.Активный).Name("ACTIVE");
+        descriptor.Value(Статус.Завершенный).Name("COMPLETED");
     }
-    
-    public static IObjectFieldDescriptor EfQueryType<TQuery>(
-        this IObjectTypeDescriptor descriptor,
-        string? name = null,
-        MiddlewareFlags middlewareFlags = MiddlewareFlags.All)
-    
-        where TQuery : ObjectType
-    {
-        // Pain point #1
-        var objectTypeType = typeof(TQuery);
-        while (!objectTypeType.IsGenericType)
-            objectTypeType = objectTypeType.BaseType!;
-        var entityType = objectTypeType.GetGenericArguments()[0];
-        
-        return (IObjectFieldDescriptor) typeof(GraphQlHelper)
-            .GetMethod("EfQuery", BindingFlags.Public | BindingFlags.Static)!
-            .MakeGenericMethod(entityType, typeof(TQuery))
-            .Invoke(null, new object?[] { descriptor, name, middlewareFlags })!;
-    }
-    
-    public static IObjectFieldDescriptor EfQuery<TEntity, TQuery>(
-        this IObjectTypeDescriptor descriptor,
-        string? name = null,
-        MiddlewareFlags middlewareFlags = MiddlewareFlags.All)
-    
-        where TQuery : ObjectType<TEntity>
-        where TEntity : class
-    {
-        name ??= GetQueryNameForType(typeof(TEntity), middlewareFlags);
-        var f = ConfigureEfQuery<TQuery>(descriptor, name, middlewareFlags);
-        return f.Resolve(ctx => ctx
-            .DbContext<ApplicationDbContext>()
-            .Set<TEntity>()
-            .AsQueryable());
-    }
-    
-    // How do we apply the convention that hot chocolate uses, instead of redefining it??
-    // Pain point #2
-    private static string GetQueryNameForType(Type entityType, MiddlewareFlags middlewareFlags)
-    {
-        var name = entityType.Name;
-        name = name[.. 1].ToLower() + name[1 ..];
-        
-        if ((middlewareFlags & MiddlewareFlags.Paging) != 0)
-            name += "Paged";
-        
-        return name;
-    }
-    
-    private static IObjectFieldDescriptor ConfigureEfQuery<TQuery>(
-        this IObjectTypeDescriptor descriptor,
-        string name,
-        MiddlewareFlags middlewareFlags)
+}
 
-        where TQuery : ObjectType
-    {
-        var f = descriptor
-            .Field(name)
-            .Type<NonNullType<ListType<NonNullType<TQuery>>>>()
-            .UseDbContext<ApplicationDbContext>();
-
-        if ((middlewareFlags & MiddlewareFlags.Paging) != 0)
-            f = f.UsePaging<NonNullType<TQuery>>();
-        if ((middlewareFlags & MiddlewareFlags.Projection) != 0)
-            f = f.UseProjection();
-        if ((middlewareFlags & MiddlewareFlags.Filtering) != 0)
-            f = f.UseFiltering();
-        if ((middlewareFlags & MiddlewareFlags.Sorting) != 0)
-            f = f.UseSorting();
-
-        return f;
-    }
-    
-    
-    public static IObjectFieldDescriptor EfQueryDto<TEntity, TDto, TQuery>(
-        this IObjectTypeDescriptor descriptor)
-    
-        where TEntity : class
-        where TDto : class
-        where TQuery : ObjectType<TDto>
-    {
-        return descriptor
-            .Field("test")
-            .Type<NonNullType<ListType<NonNullType<TQuery>>>>()
-            .UseDbContext<ApplicationDbContext>()
-            .UsePaging<NonNullType<TQuery>>()
-            .UseProjection()
-            .UseFiltering()
-            .UseSorting()
-            .Resolve(ctx => ctx
-                .DbContext<ApplicationDbContext>()
-                .Set<TEntity>()
-                .AsQueryable()
-                .ProjectTo<TEntity, TDto>(ctx));
-    }
+public class Thing
+{
+    public Статус Status { get; set; }
 }
 
 public class QueryType : ObjectType
@@ -129,103 +27,6 @@ public class QueryType : ObjectType
     {
         descriptor
             .Field("test")
-            .Type<NonNullType<ListType<NonNullType<ObjectType<GraphQlPersonDto>>>>>()
-            .UseDbContext<ApplicationDbContext>()
-            // .UsePaging<NonNullType<ObjectType<GraphQlPersonDto>>>()
-            .UseProjection()
-            .UseFiltering()
-            .UseSorting()
-            .Resolve(ctx => ctx
-                .DbContext<ApplicationDbContext>()
-                .Set<Person>()
-                .AsQueryable()
-                .ProjectTo<Person, GraphQlPersonDto>(ctx));
-        
-        descriptor
-            .Field("test2")
-            .Type<NonNullType<ListType<NonNullType<ObjectType<Person>>>>>()
-            .UseDbContext<ApplicationDbContext>()
-            // .UsePaging<NonNullType<ObjectType<GraphQlPersonDto>>>()
-            .UseProjection()
-            .UseFiltering()
-            .UseSorting()
-            .Resolve(ctx => ctx
-                .DbContext<ApplicationDbContext>()
-                .Set<Person>()
-                .AsQueryable());
-    }
-}
-
-public class ProjectDtoType : ObjectType<GraphQlProjectDto>
-{
-    protected override void Configure(IObjectTypeDescriptor<GraphQlProjectDto> descriptor)
-    {
-        descriptor.BindFieldsImplicitly();
-        // descriptor.Ignore(x => x.Name);
-    }
-}
-
-public class EfObjectType<T> : ObjectType<T>
-    where T : class
-{
-    protected readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
-    
-    public EfObjectType(IDbContextFactory<ApplicationDbContext> dbContextFactory)
-    {
-        _dbContextFactory = dbContextFactory;
-    }
-    
-    protected override void Configure(IObjectTypeDescriptor<T> descriptor)
-    {
-        descriptor.BindFieldsImplicitly();
-        //
-        // using var dbContext = _dbContextFactory.CreateDbContext();
-        // var model = dbContext.Model;
-        //
-        // IObjectFieldDescriptor CreateKeyField(IReadOnlyList<IProperty> props, Type targetEntityType)
-        // {
-        //     if (props.Count > 1)
-        //         throw new NotImplementedException();
-        //
-        //     var prop = props[0];
-        //     var pointingAtEntity = targetEntityType;
-        //     return descriptor.Field(prop.PropertyInfo!).ID(pointingAtEntity.Name);
-        // }
-        //
-        // var entityModel = model.FindEntityType(typeof(T))!;
-        //
-        // {
-        //     var key = entityModel.GetKeys().Single(k => k.IsPrimaryKey());
-        //     var props = key.Properties;
-        //     if (props.Count > 1)
-        //         throw new NotImplementedException();
-        //     
-        //     var prop = props[0];
-        //     descriptor
-        //         .Field(prop.PropertyInfo!)
-        //         .ID(typeof(T).Name);
-        //
-        //     // Nodes won't work with ef core
-        //     // https://github.com/ChilliCream/graphql-platform/issues/5966
-        //     // descriptor
-        //     //     .ImplementsNode()
-        //     //     .IdField(prop.PropertyInfo!)
-        //     //     .ResolveNode(async (ctx, id) =>
-        //     //     {
-        //     //         return ctx.DbContext<ApplicationDbContext>().Set<T>();
-        //     //     });
-        // }
-        //
-        // foreach (var key in entityModel.GetForeignKeys())
-        // {
-        //     var props = key.Properties;
-        //     if (props.Count > 1)
-        //         continue;
-        //
-        //     var prop = props[0];
-        //     descriptor
-        //         .Field(prop.PropertyInfo!)
-        //         .ID(key.PrincipalEntityType.Name);
-        // }
+            .Resolve(ctx => new Thing { Status = Статус.Активный });
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,45 +1,17 @@
-using System.Linq.Expressions;
-using AutoMapper;
 using efcore_transactions;
-using HotChocolate.Execution.Processing;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-string projectManagementConnectionString = builder.Configuration.GetConnectionString("ProjectManagement")!;
-
-void ConfigureDbContext(DbContextOptionsBuilder c)
-{
-    c.UseSqlServer(projectManagementConnectionString);
-}
-
-builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(ConfigureDbContext);
-builder.Services.AddAutoMapper(c =>
-{
-    c.AddProfile<MapperProfile>();
-    
-    var config = new MapperConfiguration(configuration =>
-    {
-        configuration.AddProfile<MapperProfile>();
-    });
-    config.AssertConfigurationIsValid();
-});
-builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
 {
     var graph = builder.Services.AddGraphQLServer();
-    graph.AddGraphQLServer();
-    graph.AddQueryType<QueryType>();
-    graph.AddProjections();
-    graph.AddFiltering();
-    graph.AddSorting();
     graph.InitializeOnStartup();
-    graph.AddTransactionScopeHandler<DefaultTransactionScopeHandler>();
-    graph.AddMutationConventions(applyToAllMutations: true);
-    // graph.AddGlobalObjectIdentification();
-    // graph.AddDirectiveType<MyDirectiveType>();
-    // graph.ModifyOptions(opt => opt.UseXmlDocumentation = true);
-    // graph.AddType<ProjectDtoType>();
+    graph.AddQueryType<QueryType>();
+    graph.AddType<StatusType>();
+    graph.AddType<Thing>();
+    graph.ModifyOptions(o =>
+    {
+        o.DefaultBindingBehavior = BindingBehavior.Explicit;
+    });
 }
 
 var app = builder.Build();
